968d5e997b6ab4574c40764b46eb7ddb66b99ff4
Jorge <719727@student.dusd.net>
2023-03-09 11:38:53 -0800

    On stampede

--- BasicGamePlayJorgeP4/Assets/Scripts/PlayerController.cs
+++ BasicGamePlayJorgeP4/Assets/Scripts/PlayerController.cs
@@ -4,21 +4,24 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float xRange = 10;
+    public float horizontalInput;
+    public float speed = 10.0f;
+
+    public GameObject projectilePrefab;
     // Start is called before the first frame update
     void Start()
     {
 
     }
 
-    public float xRange = 10;
-    public float horizontalInput;
-    public float speed = 10.0f;
+   
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             //Launch porjectile from the player
-            Instantiate(projectilePrefab, transform.position, porjectilePrefab.transform.rotation);
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
         }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
